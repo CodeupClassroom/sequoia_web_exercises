c45c69104915aa76e9a488bdb15125cc385f1f0b
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-10-11 17:13:56 -0500

    Add solutions for word problems inside the JS primitive types lesson

---
+++ js/external.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my website!");
+
+var color = prompt("What is your favorite color?");
+
+alert("Great, " + color + " is my favorite color, too!");
+
