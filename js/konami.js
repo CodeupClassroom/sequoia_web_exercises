6f04a422ad86bd2871d44cb24f6606569528b6a2
Zach Gulde <zachgulde@gmail.com>
2017-10-31 09:57:58 -0500

    Start konami code exercise

---
+++ js/konami.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// When the konami code is entered, alert a message
+
+$(document).ready(function() {
+
+    // This code runs whenever a key is pressed
+    $(document).keyup(function(e) {
+
+    });
+
+});
