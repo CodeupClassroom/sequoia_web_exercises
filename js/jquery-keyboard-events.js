d240cfc00e1dd7b7dd76aca31d567a64818303ff
Zach Gulde <zachgulde@gmail.com>
2017-10-31 09:29:57 -0500

    Finish keyboard events lesson

--- js/jquery-keyboard-events.js
+++ js/jquery-keyboard-events.js
@@ -3,6 +3,9 @@
 /**
  * `.on` - we've been looking at the shorthand methods
  *   - refactor click listeners
+ *
+ *  $(selector).on('eventname', function() {  })
+ *
  * `.off` - remove event listeners
  *
  * `.keydown`
@@ -13,12 +16,39 @@
  */
 
 $(document).ready(function() {
-    $('p').click(function(e) {
+    $('p').on('click', function(e) {
         $(this).css('font-size', '28px');
+        // // remove any and all click listeners
+        // $('p').off('click');
+
+        // // remove all event listeners
+        // $('p').off();
+
     })
 
-    $('li').dblclick(function(e) {
-        $(e.target).css('background', 'yellow');
-        $(e.target).css('color', 'pink');
+    // $('li').on('dblclick', function(e) {
+    //     $(e.target).css('background', 'yellow');
+    //     $(e.target).css('color', 'pink');
+    // })
+
+    // Keyboard Events
+
+    // $('#my-input').keydown(function() {
+    //     console.log('A key was pressed!');
+    // })
+
+    // $('#my-input').keyup(function() {
+    //     console.log('A key was pressed!');
+    // })
+
+    $('#my-input').keypress(function() {
+        console.log('A key was pressed!');
     })
+
+    $('#my-input').keyup(function(e) {
+        e.preventDefault();
+        $('#output').html(e.keyCode);
+    });
+
+
 });
